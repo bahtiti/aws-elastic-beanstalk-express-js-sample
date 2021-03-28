24e59d36af8a345301a3937f9cf17a1f303cbdd8
bahtiti <ahmed.bahtiti@gmail.com>
2021-03-28 23:39:35 +0200

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('CCNA VCP ISA'));
+app.get('/', (req, res) => res.send('ohhhhhhhhhhhhh'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
